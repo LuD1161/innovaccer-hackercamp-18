c1634758f88805b6ba1adf2d48ec53651b798efe
LuD1161 <letter2aseem@gmail.com>
2018-02-19 20:17:21 +0530

    Added Hyperlinked Model for serializer, detail view for tweet and user

--- twitter_api/api/serializers.py
+++ twitter_api/api/serializers.py
@@ -1,6 +1,4 @@
-from datetime import datetime
 from rest_framework import serializers
-from rest_framework.exceptions import APIException
 from rest_framework.fields import SerializerMethodField
 from .models import Tweets, User
 
@@ -11,7 +9,18 @@ class UserSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
-class TweetSerializer(serializers.ModelSerializer):
+class TweetSerializer(serializers.HyperlinkedModelSerializer):
+    user = serializers.HyperlinkedRelatedField(
+        many=False,
+        view_name='users-detail',
+        queryset=User.objects.all()
+    )
+    username = SerializerMethodField()
+
+    def get_username(self, obj):
+        # print(type(obj))
+        return obj.user.name
+
     class Meta:
         model = Tweets
         fields = "__all__"
--- twitter_api/api/urls.py
+++ twitter_api/api/urls.py
@@ -8,8 +8,16 @@ tweet_list = TweetViewSet.as_view({
     'get': 'list'
 })
 
+tweet_detail = TweetViewSet.as_view({
+    'get': 'retrieve'
+})
+
 user_list = UserViewSet.as_view({
     'get': 'list'
+})
+
+user_detail = UserViewSet.as_view({
+    'get': 'retrieve'
 })
 
 router = DefaultRouter()
@@ -21,3 +29,4 @@ urlpatterns = [
     url(r'^query/$', views.get_track, name='query'),
     url(r'^', include(router.urls))
 ]
+print(router.urls)
